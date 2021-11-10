76d6ca8f63a1f0a77dbe55df045644164e5bf815
premnath <premdevapp@gmail.com>
2021-11-10 10:26:44 +0530

    reduxtoolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
+import { RootState } from "./app/store";
+import CustomerCard from "./components/CustomerCard";
+import ResevartionCard from "./components/ResevartionCard";
+import { addReservation } from "./features/reservationSlice";
 
 function App() {
+  const [reservationNameInput, setReservationNameInput] = useState("");
+
+  const resevartions = useSelector(
+    (state: RootState) => state.reservations.value
+  );
+
+  const customers = useSelector(
+    (state: RootState) => state.customers.value
+  );
+
+  const dispatch = useDispatch();
+
+  const handleAddReservations = () => {
+    if (!reservationNameInput) return;
+    dispatch(addReservation(reservationNameInput));
+    setReservationNameInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="reservation-container">
+          <div>
+            <h5 className="reservation-header">Reservations</h5>
+            <div className="reservation-cards-container">
+              {resevartions.map((reservation, index) => (
+                <ResevartionCard name={reservation} index={index} key={index} />
+              ))}
+            </div>
+          </div>
+          <div className="reservation-input-container">
+            <input
+              value={reservationNameInput}
+              onChange={(e) => {
+                setReservationNameInput(() => e.target.value);
+              }}
+            />
+            <button onClick={handleAddReservations}>Add</button>
+          </div>
+        </div>
+        <div className="customer-food-container">
+          {customers.map(customer => <CustomerCard {...customer} key={customer.id} />
+           )}
+        </div>
+      </div>
     </div>
   );
 }
